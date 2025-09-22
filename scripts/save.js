dbeeb34d6b9850189a2efb44c69eec8064dc8d13
Valentin Hassinen <darkfrobozz.mega@gmail.com>
2025-09-22 19:48:55 +0200

    added direct functions for save.js

--- scripts/save.js
+++ scripts/save.js
@@ -4,16 +4,43 @@
 
 // Provide functions to be used by other scripts
 window.save = {
-    get(team_name) {
-        const json = localStorage.getItem(team_name);
-        return json ? JSON.parse(json) : {}; // return empty object if not set
-    },
+  get(team_name) {
+    const json = localStorage.getItem(team_name);
+    return json ? JSON.parse(json) : {}; // return empty object if not set
+  },
 
-    set(team_name, data) {
-        localStorage.setItem(team_name, JSON.stringify(data));
-    },
+  set(team_name, data) {
+    localStorage.setItem(team_name, JSON.stringify(data));
+  },
 
-    clear(team_name) {
-        localStorage.removeItem(team_name);
-    }
+  clear(team_name) {
+    localStorage.removeItem(team_name);
+  },
 };
+
+/**
+ *
+ * @param {string} team_name
+ * @returns {Object}
+ */
+export function get(team_name) {
+  const json = localStorage.getItem(team_name);
+  return json ? JSON.parse(json) : {}; // return empty object if not set
+}
+
+/**
+ *
+ * @param {string} team_name
+ * @param {Object} data
+ */
+export function set(team_name, data) {
+  localStorage.setItem(team_name, JSON.stringify(data));
+}
+
+/**
+ *
+ * @param {string} team_name
+ */
+export function clear(team_name) {
+  localStorage.removeItem(team_name);
+}
